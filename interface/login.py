59f4f6ba0d3abe1c9bfe3e848382bda3f074039f
bugpz <13558622779@163.com>
2022-08-16 17:03:10 +0800

    feat：login package

---
+++ interface/login.py
@@ -0,0 +1,7 @@
+import requests
+from utils.get_keyword import GetKeyword
+
+def token():
+    res = requests.post('http://xxx/api/user/login',json={'phone': 'xxx', 'password': '2d3383fa392936ad7847c50a0bb4a58e'})
+    result = GetKeyword()
+    return result.get_keyword(res.json(), 'token')
