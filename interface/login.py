461dfa9daa2310be0099e3c199bebc043f391e6d
bugpz <13558622779@163.com>
2022-08-21 12:02:53 +0800

    refactor：change dir_name

--- interface/login.py
+++ interface/login.py
@@ -1,5 +1,5 @@
 import requests
-from utils.get_keyword import GetKeyword
+from common.get_keyword import GetKeyword
 
 def token():
     res = requests.post('http://xxx/api/user/login',json={'phone': 'xxx', 'password': '2d3383fa392936ad7847c50a0bb4a58e'})
