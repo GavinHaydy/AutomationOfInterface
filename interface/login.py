31f0c23bec22b3e751a138c2097bbca485c5614f
bugpz <13558622779@163.com>
2022-08-21 23:14:42 +0800

    feat：func of token

--- interface/login.py
+++ interface/login.py
@@ -1,7 +1,8 @@
 import requests
 from common.get_keyword import GetKeyword
+from config import glo_vars as glo
 
 def token():
-    res = requests.post('http://xxx/api/user/login',json={'phone': 'xxx', 'password': '2d3383fa392936ad7847c50a0bb4a58e'})
+    res = requests.post( f'{glo.url_glo()}/api/user/login',json={'phone': 'xxx', 'password': '2d3383fa392936ad7847c50a0bb4a58e'})
     result = GetKeyword()
     return result.get_keyword(res.json(), 'token')
