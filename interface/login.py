da8d41d3b527e0ba5b5ac4a6b3d42d34266a1161
GavinHaydy <13558622779@163.com>
2024-08-05 16:59:47 +0800

    perf：优化示例

--- interface/login.py
+++ interface/login.py
@@ -6,14 +6,12 @@
 
 import requests
 from common.get_keyword import GetKeyword
-from config.glo_vars import url_glo
-
-from common.document_operation import read_yaml
+from common.conf import read_yaml
 
 
 def token():
-    data = ["uri", "phone", "password"]
+    data = ["username", "password"]
     login_pat = eval(f"{read_yaml('../config/env.yaml')['login']}")
-    res = requests.request(login_pat['method'].lower(), f'{url_glo()}' + login_pat['uri'],
+    res = requests.request(login_pat['method'].lower(), login_pat['url'],
                            json={key: val for key, val in login_pat.items() if key in data}).json()
     return GetKeyword.get_keyword(res, login_pat['token_name'])
